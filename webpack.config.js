e3ded0b41e49c64724f2b74bc52d0d90ac1c46a0
ParthPatel2000 <arjun.823821@gmail.com>
2025-07-07 19:16:57 -0400

    ui changes and quality of life updates.

--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,3 @@
-// const path = require('path');
-// const CopyPlugin = require('copy-webpack-plugin');
-
-// module.exports = {
-//     mode: 'production',
-//     entry: {
-//         background: './src/background.js',
-//         content: './src/content.js',
-//         'popup/popup': './src/popup/popup.js',
-//     },
-//     output: {
-//         path: path.resolve(__dirname, 'dist'),
-//         filename: '[name].js',
-//     },
-//     plugins: [
-//         new CopyPlugin({
-//             patterns: [
-//                 { from: 'manifest.json', to: '' },  // Copies manifest.json to dist/
-//                 { from: 'src/popup/popup.html', to: 'popup' }  // popup.html copy into dist/popup/
-//             ],
-//         }),
-//     ],
-//     resolve: {
-//         fallback: {
-//             crypto: false,
-//             stream: false,
-//         },
-//     },
-//     experiments: {
-//         topLevelAwait: true, // Needed if you use top-level await (Firebase sometimes needs it)
-//     },
-// };
-
-
 const path = require('path');
 const CopyPlugin = require('copy-webpack-plugin');
 const webpack = require('webpack');
@@ -56,6 +22,7 @@ module.exports = (env, argv) => {
                 patterns: [
                     { from: 'manifest.json', to: '' },
                     { from: 'src/popup/popup.html', to: 'popup' },
+                    { from: 'src/icons', to: 'icons' }, 
                 ],
             }),
 
